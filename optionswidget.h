0c5a8ab17904caa07d066dddb174346cb2c03e53
WuTuT <762641171@qq.com>
2018-06-24 05:43:04 +0800

    ruaruarua

---
+++ optionswidget.h
@@ -0,0 +1,17 @@
+#ifndef OPTIONSWIDGET_H
+#define OPTIONSWIDGET_H
+
+#include <QWidget>
+
+class OptionsWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit OptionsWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // OPTIONSWIDGET_H
